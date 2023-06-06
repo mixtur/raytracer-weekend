6abd3f634a7dee30be46971077437f8136b24fdb
mixture <turkeevm@gmail.com>
2023-06-06 23:52:42 +0400

    add a sphere

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,17 +1,42 @@
 import { writeColor } from "./color";
 import { ray, Ray } from "./ray";
-import { color, vec3, vec3Mix3, vec3Unit1, Color, vec3Mix4, point3, vec3DivS2, vec3Add2, vec3Sub2, vec3MulS2 } from "./vec3";
+import {
+    color,
+    vec3,
+    vec3Mix3,
+    vec3Unit1,
+    Color,
+    vec3Mix4,
+    point3,
+    vec3DivS2,
+    vec3Add2,
+    vec3Sub2,
+    vec3MulS2,
+    Point3, vec3Dot
+} from './vec3';
 
 const canvas = document.createElement('canvas');
 const ctx = canvas.getContext('2d');
 document.body.appendChild(canvas);
 
-const rayColor = (r: Ray): Color => {
+const hit_sphere = (center: Point3, radius: number, r: Ray): boolean => {
+    const oc = vec3Sub2(r.origin, center);
+    const a = vec3Dot(r.direction, r.direction);
+    const b = 2 * vec3Dot(oc, r.direction);
+    const c = vec3Dot(oc, oc) - radius * radius;
+    const D = b * b - 4 * a * c;
+    return D > 0;
+}
+
+const ray_color = (r: Ray): Color => {
+    if (hit_sphere(point3(0, 0, -1), 0.5, r)) {
+        return color(1, 0, 0);
+    }
     const unitDirection = r.direction.slice();
     const t = 0.5 * (unitDirection[1] + 1);
     vec3Mix4(unitDirection, color(1, 1, 1), color(0.5, 0.7, 1), t);
     return unitDirection;
-}
+};
 
 
 
@@ -50,7 +75,7 @@ for (let j = 0; j < image_height; j++) {
 
         const r = ray(origin, vec3Sub2(vec3Add2(lower_left_corner, vec3Add2(vec3MulS2(horizontal, u), vec3MulS2(vertical, v))), origin));
 
-        const pixelColor = rayColor(r);
+        const pixelColor = ray_color(r);
         writeColor(imageData, x, y, pixelColor);
     }
 }
