93e57a8fb25cae2ccdc4300830210dbfe78e1983
mixture <turkeevm@gmail.com>
2023-06-23 00:19:16 +0400

    move allocator scope higher

--- src/js/index.ts
+++ src/js/index.ts
@@ -57,15 +57,15 @@ async function main() {
     const scene = cornell_box;
     const cam = scene.create_camera(aspect_ratio);
 
-    for (let j = 0; j < image_height; j++) {
-        const mark = `scanline remaining ${image_height - j - 1}`;
-        const y = image_height -1 - j;
-        console.time(mark);
-        for (let i = 0; i < image_width; i++) {
-            const x = i;
+    await vec3AllocatorScope(rayArenaAllocator, async () => {
+        for (let j = 0; j < image_height; j++) {
+            const mark = `scanline remaining ${image_height - j - 1}`;
+            const y = image_height -1 - j;
+            console.time(mark);
+            for (let i = 0; i < image_width; i++) {
+                const x = i;
 
-            const pixelColor = color(0, 0, 0);
-            vec3AllocatorScope(rayArenaAllocator, () => {
+                const pixelColor = color(0, 0, 0);
                 for (let s = 0; s < samples_per_pixel; s++) {
                     rayArenaAllocator.reset();
                     const u = (i + Math.random()) / (image_width - 1);
@@ -74,13 +74,13 @@ async function main() {
                     const r = cam.get_ray(u, v);
                     vec3Add3(pixelColor, pixelColor, ray_color(r, scene.background, scene.root_hittable, max_depth));
                 }
-            });
-            writeColor(x, y, pixelColor, samples_per_pixel);
+                writeColor(x, y, pixelColor, samples_per_pixel);
+            }
+            console.timeEnd(mark);
+            await new Promise(resolve => setTimeout(resolve, 0));
+            dumpLine(y);
         }
-        console.timeEnd(mark);
-        await new Promise(resolve => setTimeout(resolve, 0));
-        dumpLine(y);
-    }
+    });
     dumpImage();
     console.log('Done!');
 }
