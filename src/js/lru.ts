f3741a0690f24022c1110f18c1c259af74eff924
Mikhail Turkeev <turkeevm@gmail.com>
2024-08-05 10:56:47 +0400

    clean comments in LRU

--- src/js/lru.ts
+++ src/js/lru.ts
@@ -9,16 +9,12 @@ export class LRUCache<Key, Value, UpdateInfo> {
     fresh: LRUNode<Key, Value> | undefined = undefined;
     stale: LRUNode<Key, Value> | undefined = undefined;
 
-    // storage: (LRUNode<Value> | undefined)[] = [];
     index = new Map<Key, LRUNode<Key, Value>>();
     empty_space: number;
 
     constructor(private capacity: number, private create: () => Value, private reuse: (value: Value, update_info: UpdateInfo) => void) {
         console.assert(capacity >= 1);
         this.empty_space = capacity;
-        // for (let i = 0; i < capacity; i++) {
-        //     this.storage.push(undefined);
-        // }
     }
 
     get(key: Key, update_info: UpdateInfo): Value {
@@ -68,11 +64,11 @@ export class LRUCache<Key, Value, UpdateInfo> {
             prev.fresh = node.fresh;
         }
         next.stale = node.stale;
-        // at this point node is not a part of the list, need to push it to attach it to fresh end
+        // at this point node is not a part of the list, need to move it to the fresh end
 
         node.stale = this.fresh
         node.fresh = undefined
-        //can assert because before mtt node, was a part of list, and it also wasn't this.fresh, so there were at least 2 elements in the list
+        //can assert because mtt is never called with empty cache
         this.fresh!.fresh = node
         this.fresh = node
     }
